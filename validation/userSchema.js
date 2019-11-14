d5aa0eafe281b893b61066ae4f2e25ffd5f23387
osamaakb <osama98akb@gmail.com>
2019-11-14 11:50:56 +0300

    put back the admin regex in userSchema

--- validation/userSchema.js
+++ validation/userSchema.js
@@ -5,7 +5,14 @@ const Joi = require('@hapi/joi')
 const userSchema = {
     signup: Joi.object().keys({
         name: Joi.string().required().min(3),
-        email: Joi.string().required().email(),
+        email: Joi.string().required().email().regex(/^.+@workwithus\.com$/, { invert: true }).error(errors => {
+            errors.forEach(error => {
+                if(error.code === 'string.pattern.invert.base'){
+                    error.message = 'you are not premitted to sign up with this email'
+                }
+            });
+            return errors
+        }),
         password: Joi.string().min(8).max(30).required()
     }),
     login: Joi.object().keys({
