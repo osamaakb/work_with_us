ac58a1af5e384b51cc0e9cb82d7e23a790f150c3
osamaakb <osama98akb@gmail.com>
2019-12-27 14:12:45 +0300

    remove cv required from portfolio scheme

--- validation/portfolioSchema.js
+++ validation/portfolioSchema.js
@@ -1,24 +1,29 @@
-const Joi = require('@hapi/joi')
-
+const Joi = require("@hapi/joi");
 
 const portfolioSchema = {
-    query : Joi.object().keys({
-        area_id: Joi.number().integer(),
-        category_id: Joi.number().integer(),
-        afterId: Joi.number().integer()
-    }),
-    portfolio : Joi.object().unknown().keys({
-        area_id: Joi.number().integer().required(),
-        category_id: Joi.number().integer().required(),
-        title: Joi.string().required(),
-        name: Joi.string().required(),
-        phone: Joi.number().integer().required(),
-        cv: Joi.string().required(),
-        posted_at: Joi.any(),
-        created_at: Joi.any(),
+  query: Joi.object().keys({
+    area_id: Joi.number().integer(),
+    category_id: Joi.number().integer(),
+    afterId: Joi.number().integer()
+  }),
+  portfolio: Joi.object()
+    .unknown()
+    .keys({
+      area_id: Joi.number()
+        .integer()
+        .required(),
+      category_id: Joi.number()
+        .integer()
+        .required(),
+      title: Joi.string().required(),
+      name: Joi.string().required(),
+      phone: Joi.number()
+        .integer()
+        .required(),
+      // cv: Joi.string().required(),
+      posted_at: Joi.any(),
+      created_at: Joi.any()
     })
-}
-
-
+};
 
 module.exports = portfolioSchema;
